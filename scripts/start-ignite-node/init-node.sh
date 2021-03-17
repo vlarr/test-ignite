608bb054083731f399b13f18364ad6eb9fd0d231
vrogozin <vvrogozin@vrn.beeline.ru>
2021-03-17 12:28:22 +0300

    update ignite version to 2.10.0

--- scripts/start-ignite-node/init-node.sh
+++ scripts/start-ignite-node/init-node.sh
@@ -1,2 +1,2 @@
-docker run -v "$(pwd)/my-conf.xml:/my-conf.xml" -e "OPTION_LIBS=ignite-rest-http,ignite-aws" -e "CONFIG_URI=/my-conf.xml" -p 8080:8080 -p 47500:47500 apacheignite/ignite:2.9.1
+docker run -v "$(pwd)/my-conf.xml:/my-conf.xml" -e "OPTION_LIBS=ignite-rest-http,ignite-aws" -e "CONFIG_URI=/my-conf.xml" -p 8080:8080 -p 47500:47500 apacheignite/ignite:2.10.0
 
